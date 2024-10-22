de55788f09ce2dfe5193ca12adb4e9b1c927ccd4
RAFAEL CÉSAR PRESTES RAPOSO <rafailusion@hotmail.com>
2024-10-22 17:14:01 +0000

    adicionando contatenação

---
+++ concatenando.py
@@ -0,0 +1,9 @@
+# Solicita dois dados do usuário
+dado1 = input("Digite o primeiro dado: ")
+dado2 = input("Digite o segundo dado: ")
+
+# Concatena os dados
+resultado = dado1 + " " +dado2
+
+# Exibe o resultado
+print("Resultado concatenado:", resultado)
